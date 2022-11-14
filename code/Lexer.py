b00e6a13e1195dfc617ece23ee968c0c4f89c533
Kaio Sodré <kaio.sodc@gmail.com>
2022-11-14 20:46:16 -0300

    add automatos

--- code/Lexer.py
+++ code/Lexer.py
@@ -5,11 +5,13 @@ class Lexer:
         self.list_identifier = []
         self.list_operator= []
         self.list_keyWord = []
+        self.list_delimiter = []
         self.inp = inp
 
-        self.dict_lex = {'Identifier':self.list_identifier,
-                        'Operator':self.list_operator,
-                        'Key World':self.list_keyWord}
+        self.dict_lex = {'Identificador':self.list_identifier,
+                        'Operador':self.list_operator,
+                        'Palavra Chave':self.list_keyWord,
+                        'Delimitador':self.list_delimiter}
 
     def identifier_tokens(self):
         list_inp = self.inp.split('\n')
@@ -20,13 +22,34 @@ class Lexer:
             list_lexemas = re.split("\s", line)
             list_lexemas = list(filter(None, list_lexemas))
             print(list_lexemas)
-            
+
             for i in list_lexemas:
+                c = 0
                 if re.match('[A-Z]+|[a-z]+',i) != None:
-                    self.list_identifier.append(i)
+                    for j in ['numb','liter','boole','if','else','true','false','null','show','doit','loop','case','orcase','other']:
+                        if i == j:
+                            c = 1
+                            self.list_keyWord.append(i)
+                            break
+                    if c != 1:  
+                        self.list_identifier.append(i)
+                            
                 elif re.match('[+|\-|\/|*|=]',i) != None:
                     self.list_operator.append(i)
-                elif i == 'liter' or i == 'numb' or i == 'show':
-                    self.list_keyWord.append(i)
+
+                elif re.match('[,|.||(|)]',i) != None:
+                    self.list_delimiter.append(i)
+
+        # list(set(lista))
+
+        self.list_keyWord = list(set(self.list_keyWord))
+        self.list_identifier = list(set(self.list_identifier))
+        self.list_operator = list(set(self.list_operator))
+
+        self.dict_lex = {'Identificador':self.list_identifier,
+                        'Operador':self.list_operator,
+                        'Palavra Chave':self.list_keyWord,
+                        'Delimitador':self.list_delimiter}
+
 
         return self.dict_lex
--- code/main.py
+++ code/main.py
@@ -1,16 +1,29 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+import webbrowser
 from Lexer import Lexer
 
 root = Tk()
 style = ttk.Style()
 style.theme_use('clam')
 
-root.geometry("700x400") 
+style.configure("Treeview",
+    background = "#D3D3D3",
+    foreground = "black",
+    rowheight =25,
+    fieldbackground = "#D3D3D3"
+)
+
+style.map('Treeview',background=[('selected','blue')])
+
+root.geometry("900x500") 
 root.configure(background="#a0a0a0")
-root.title('Lexical Analyzer')
+root.title('Mali')
 root.resizable(width=False, height=False)
 
+
+# Commands
 def getInput(): 
     inp = inputText.get(1.0, "end-1c")
     analyzer = Lexer(inp) 
@@ -27,33 +40,69 @@ def clearInput():
     for i in table.get_children():
         table.delete(i)
 
+def showInfo():
+    messagebox.showinfo(title='About', message='This is an IDE for Mali Language')
+
+
     
 # root_label = Label(root, text="Analisador Lexico", font=15) 
 # root_label.place(x=390,y=300)
 
 #Box Text
-inputText = Text(root,height = 24, width = 53,font=("Cascade Mono",10))
-inputText.place(x=5,y=5)
+inputText = Text(root,height = 30, width = 60,font=("Cascade Mono",10))
+inputText.place(x=5,y=6)
 # inp = inputText.get(1.0, "end-1c") 
 
-# Buttons
-button = ttk.Button(root, text = ' Start ',command = getInput)  
-button.place(x=430,y=350)  
+# Menu Bar
+barMenu = Menu(root)
 
-button = ttk.Button(root, text = ' Clear ',command = clearInput)  
-button.place(x=520,y=350)  
+# Menu Bar - Files
+menuFile = Menu(barMenu,tearoff=0)
+menuFile.add_command(label='Add File',command=getInput)
+
+# Menu Bar - Run
+menuRun = Menu(barMenu,tearoff=0)
+menuRun.add_command(label='Run Code',command=getInput)
+
+# Menu Bar - Help
+menuHelp = Menu(barMenu,tearoff=0)
+menuHelp.add_command(label='GitHub',
+                     command=lambda: webbrowser.open('https://github.com/kaiosod/compiler'))
+menuHelp.add_command(label='Issues',
+                     command=lambda: webbrowser.open('https://github.com/kaiosod/compiler/issues'))
+menuHelp.add_command(label='Documentarion',
+                     command=lambda: webbrowser.open('https://github.com/kaiosod/compiler/wiki'))
+menuHelp.add_command(label='About',
+                     command=showInfo)
+
+
+# menuFile.add_separator()
+barMenu.add_cascade(label='Files',menu=menuFile)
+barMenu.add_cascade(label='Run',menu=menuRun)
+barMenu.add_cascade(label='Help',menu=menuHelp)
+
+
+
+root.config(menu=barMenu)
+
+# Buttons
+button = ttk.Button(root, text = 'Start',command = getInput)  
+button.place(x=435,y=290)  
+
+button = ttk.Button(root, text = 'Clear',command = clearInput)  
+button.place(x=435,y=325)  
 
 #Table
 table = ttk.Treeview(root,column = ['column1','column2'],show = 'headings')
 
-table.column('column1',width=150)
+table.column('column1',width=120)
 table.heading('#1',text = 'Token')
 
-table.column('column2',width=150)
+table.column('column2',width=335)
 table.heading('#2',text = 'Lexema')
 
 table.grid(row = 0, column=0)
-table.place(x=390,y=5)
+table.place(x=435,y=5)
 
  
 mainloop() 
