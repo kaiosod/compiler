a2cb9a02f9f7fa37dbe8aceb51010afc3daa6609
Kaio Sodré <kaio.sodc@gmail.com>
2022-11-15 10:53:16 -0300

    fix bugs and remove comments

--- code/Lexer.py
+++ code/Lexer.py
@@ -14,19 +14,39 @@ class Lexer:
                         'Delimitador':self.list_delimiter}
 
     def identifier_tokens(self):
+
         list_inp = self.inp.split('\n')
 
         for line in list_inp:
-            
-            # list_lexemas = line.split(' ')
-            list_lexemas = re.split("\s", line)
-            list_lexemas = list(filter(None, list_lexemas))
-            print(list_lexemas)
 
+            line = self.remove_comments(line)
+            line = self.remove_garbage(line)
+            
+                        
+            # Does not consider space
+            for i in line:
+                if re.match('[+|\-|\/|*|=]',i) != None:
+                    self.list_operator.append(i)
+                    line = line.replace(i,'')
+
+                elif re.match('[,|.||(|)]',i) != None:
+                    self.list_delimiter.append(i)
+
+            # Remove Args of functions
+            line = self.remove_args(line)
+
+            list_lexemas = re.split("\s", line)
+
+            # Drop duplicates
+            list_lexemas = list(filter(None, list_lexemas))
+
+            
+            # Consider space
             for i in list_lexemas:
                 c = 0
+
                 if re.match('[A-Z]+|[a-z]+',i) != None:
-                    for j in ['numb','liter','boole','if','else','true','false','null','show','doit','loop','case','orcase','other']:
+                    for j in ['numb','liter','boole','if','else','true','false','null','show','doit','loop','case','orcase','other','range','in']:
                         if i == j:
                             c = 1
                             self.list_keyWord.append(i)
@@ -34,17 +54,11 @@ class Lexer:
                     if c != 1:  
                         self.list_identifier.append(i)
                             
-                elif re.match('[+|\-|\/|*|=]',i) != None:
-                    self.list_operator.append(i)
-
-                elif re.match('[,|.||(|)]',i) != None:
-                    self.list_delimiter.append(i)
-
-        # list(set(lista))
-
+   
         self.list_keyWord = list(set(self.list_keyWord))
         self.list_identifier = list(set(self.list_identifier))
         self.list_operator = list(set(self.list_operator))
+        self.list_delimiter = list(set(self.list_delimiter))
 
         self.dict_lex = {'Identificador':self.list_identifier,
                         'Operador':self.list_operator,
@@ -53,3 +67,31 @@ class Lexer:
 
 
         return self.dict_lex
+
+    def remove_comments(self,line):
+        if re.findall('//',line) != None:
+            
+            try:
+                line_pos = re.search('//',line)
+                line_comment = line[line_pos.span()[0]:]
+                out = line.replace(line_comment,'')
+            except:
+                out = line
+                pass
+            
+        return out
+
+    def remove_args(self,line):
+        pos1 = line.find('(')
+        pos2 = line.find(')')
+        out = line.replace(line[pos1:pos2+1],'')
+        return out
+
+    def remove_garbage(self,line):
+        try:
+            out = line.replace(':','')
+        except:
+            pass
+        return out
+
+
